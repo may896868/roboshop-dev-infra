c96208db463028c14b190153b02576552711ec8b
may896868 <may896868@gmail.com>
2025-11-03 15:31:44 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/may896868/ansible-roboshop-roles-tf.git  component=mongodb main.yaml
+ansible-pull -U https://github.com/may896868/ansible-roboshop-roles-tf.git -e component=$component main.yaml
 #ansible-pull -U https://github.com/may896868/ansible-roboshop-roles-tf.git mongodb.yaml
 
