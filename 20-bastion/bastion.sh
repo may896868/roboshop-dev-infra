ee5629fb7adc8da1475feccbaef218c359d3fafa
may896868 <may896868@gmail.com>
2025-11-04 10:08:00 +0530

    terraform

---
+++ 20-bastion/bastion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# installing terraform at the time bastion host creation using user_data calling
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
+
