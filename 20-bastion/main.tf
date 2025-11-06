ebfefd4e4312cb897f22cde03b5365953d2fb5b9
may896868 <may896868@gmail.com>
2025-11-06 10:12:52 +0530

    terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
+    
     tags = merge(
         local.common_tags,
         {
@@ -13,3 +14,7 @@ resource "aws_instance" "bastion" {
     )
 }
 
+resource "aws_iam_instance_profile" "bastion" {
+  name = "mysql"
+  role = "BastionTerraformAdmin"
+}
